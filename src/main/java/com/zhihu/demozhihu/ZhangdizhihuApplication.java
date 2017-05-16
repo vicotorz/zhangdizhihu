a86b7a22a2493fc8b1f48114099bc928c2c924e1
Victorz <victorzsnail@qq.com>
2017-05-16 14:33:41 +0800

    VictorzWenda

---
+++ src/main/java/com/zhihu/demozhihu/ZhangdizhihuApplication.java
@@ -0,0 +1,12 @@
+package com.zhihu.demozhihu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhangdizhihuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhangdizhihuApplication.class, args);
+    }
+}
