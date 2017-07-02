d7b0cc04e81c34f6dd79e556080318dfc8d3edf1
Victorz <victorzsnail@qq.com>
2017-07-02 23:39:17 +0800

    add FollowService

--- src/main/java/com/zhihu/Utils/JedisAdapter.java
+++ src/main/java/com/zhihu/Utils/JedisAdapter.java
@@ -6,49 +6,53 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by dell on 2017/6/24.
  */
 @Service
 public class JedisAdapter implements InitializingBean {
-    private static final Logger logger= LoggerFactory.getLogger(JedisAdapter.class);
+    private static final Logger logger = LoggerFactory.getLogger(JedisAdapter.class);
     private JedisPool pool;
     private Jedis jedis;
+
     @Override
     public void afterPropertiesSet() throws Exception {
-        pool=new JedisPool("redis://localhost:6379");
+        pool = new JedisPool("redis://localhost:6379");
         System.out.println("初始化jedispool");
     }
 
     //Set添加
-    public long sadd(String key,String value){
+    public long sadd(String key, String value) {
         Jedis jedis = null;
         try {
             jedis = pool.getResource();
-            return jedis.sadd(key,value);
-        }catch(Exception e){
-            logger.error("Jedis--sadd方法异常"+e.getMessage());
-        }finally {
-            if(jedis!=null){
+            return jedis.sadd(key, value);
+        } catch (Exception e) {
+            logger.error("Jedis--sadd方法异常" + e.getMessage());
+        } finally {
+            if (jedis != null) {
                 jedis.close();
             }
         }
-       return 0;
+        return 0;
     }
 
     //Set删除
-    public long srem(String key,String value){
+    public long srem(String key, String value) {
         Jedis jedis = null;
-        try{
-            jedis=pool.getResource();
-            return jedis.srem(key,value);
-        }catch (Exception e){
-            logger.error("Jedis--srem方法异常"+e.getMessage());
-        }finally{
-            if(jedis!=null){
+        try {
+            jedis = pool.getResource();
+            return jedis.srem(key, value);
+        } catch (Exception e) {
+            logger.error("Jedis--srem方法异常" + e.getMessage());
+        } finally {
+            if (jedis != null) {
                 jedis.close();
             }
         }
@@ -56,16 +60,16 @@ public class JedisAdapter implements InitializingBean {
     }
 
     //Set弹出
-    public long scard(String key){
+    public long scard(String key) {
         Jedis jedis = null;
         System.out.println("scard");
-        try{
-            jedis=pool.getResource();
+        try {
+            jedis = pool.getResource();
             return jedis.scard(key);
-        }catch (Exception e){
-            logger.error("Jedis--scard方法异常"+e.getMessage());
-        }finally{
-            if(jedis!=null){
+        } catch (Exception e) {
+            logger.error("Jedis--scard方法异常" + e.getMessage());
+        } finally {
+            if (jedis != null) {
                 jedis.close();
             }
         }
@@ -73,16 +77,16 @@ public class JedisAdapter implements InitializingBean {
     }
 
     //Set是否是一个成员
-    public boolean sismember(String key,String value){
+    public boolean sismember(String key, String value) {
         Jedis jedis = null;
         System.out.println("sismember方法调用");
-        try{
-            jedis=pool.getResource();
-            return jedis.sismember(key,value);
-        }catch (Exception e){
-            logger.error("Jedis--sismember异常"+e.getMessage());
-        }finally{
-            if(jedis!=null){
+        try {
+            jedis = pool.getResource();
+            return jedis.sismember(key, value);
+        } catch (Exception e) {
+            logger.error("Jedis--sismember异常" + e.getMessage());
+        } finally {
+            if (jedis != null) {
                 jedis.close();
             }
         }
@@ -90,18 +94,18 @@ public class JedisAdapter implements InitializingBean {
     }
 
     //阻塞队列弹出元素
-    public List<String> brpop(int timeout, String key){
+    public List<String> brpop(int timeout, String key) {
         Jedis jedis = null;
         System.out.println("brpop方法调用");
-        try{
+        try {
             System.out.println("获得连接");
-            jedis=pool.getResource();
+            jedis = pool.getResource();
             System.out.println("获得连接，brpop");
-            return jedis.brpop(timeout,key);
-        }catch (Exception e){
-            logger.error("Jedis--brpop异常"+e.getMessage());
-        }finally{
-            if(jedis!=null){
+            return jedis.brpop(timeout, key);
+        } catch (Exception e) {
+            logger.error("Jedis--brpop异常" + e.getMessage());
+        } finally {
+            if (jedis != null) {
                 jedis.close();
             }
         }
@@ -109,20 +113,122 @@ public class JedisAdapter implements InitializingBean {
     }
 
     //List添加
-    public long lpush(String key,String value){
+    public long lpush(String key, String value) {
         Jedis jedis = null;
-        try{
-            System.out.println(key+"---"+value);
-            jedis=pool.getResource();
-            return jedis.lpush(key,value);
-        }catch (Exception e){
-            logger.error("Jedis--lpush异常"+e.getMessage());
+        try {
+            System.out.println(key + "---" + value);
+            jedis = pool.getResource();
+            return jedis.lpush(key, value);
+        } catch (Exception e) {
+            logger.error("Jedis--lpush异常" + e.getMessage());
             e.printStackTrace();
-        }finally{
-            if(jedis!=null){
+        } finally {
+            if (jedis != null) {
                 jedis.close();
             }
         }
         return 0;
     }
+
+    ////////增加jedis事务操作
+    public Jedis getJedis() {
+        return pool.getResource();
+    }
+
+    public Transaction multi(Jedis jedis) {
+        try {
+            return jedis.multi();
+        } catch (Exception e) {
+            logger.error("发生异常" + e.getMessage());
+        } finally {
+
+        }
+        return null;
+    }
+    //Transcation
+    public List<Object> exec(Transaction tx, Jedis jedis) {
+        try {
+           return tx.exec();
+        } catch (Exception e) {
+            logger.error("发生异常" + e.getMessage());
+            tx.discard();
+        } finally {
+            if (tx != null) {
+                try {
+                    tx.close();
+                } catch (IOException ioe) {
+                }
+            }
+
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+
+    //从小到大排名
+    public Set<String> zrange(String key, int start, int end){
+        Jedis jedis=null;
+        try{
+                 jedis=pool.getResource();
+                 return jedis.zrange(key,start,end);
+        }catch (Exception e){
+            logger.error("发生异常" + e.getMessage());
+        }finally{
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+        return null;
+    }
+    //从大到小排名
+    public Set<String> zrevrange(String key, int start, int end){
+        Jedis jedis=null;
+        try{
+            jedis=pool.getResource();
+            return jedis.zrevrange(key,start,end);
+
+        }catch (Exception e){
+            logger.error("发生异常" + e.getMessage());
+        }finally{
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+     //有序元素个数
+    public long zcard(String key){
+        Jedis jedis=null;
+        try{
+         jedis=pool.getResource();
+         return jedis.zcard(key);
+        }catch (Exception e){
+            logger.error("发生异常" + e.getMessage());
+        }finally{
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+        return 0;
+    }
+    //得打score
+    public Double zscore(String key, String number){
+        Jedis jedis=null;
+        try{
+            jedis=pool.getResource();
+            return jedis.zscore(key,number);
+        }catch (Exception e){
+            logger.error("发生异常" + e.getMessage());
+        }finally{
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
 }
--- src/main/java/com/zhihu/Utils/RedisKeyUtil.java
+++ src/main/java/com/zhihu/Utils/RedisKeyUtil.java
@@ -9,6 +9,14 @@ public class RedisKeyUtil {
     private static String BIZ_DISLIKE = "DISLIKE";
     private static String BIZ_EVENTQUEUE = "EVENT_QUEUE";
 
+    // 获取粉丝
+    private static String BIZ_FOLLOWER = "FOLLOWER";
+    // 关注对象
+    private static String BIZ_FOLLOWEE = "FOLLOWEE";
+    private static String BIZ_TIMELINE = "TIMELINE";
+
+
+
     //得到喜欢的串
     public static String getLikeKey(int entityType, int entityId) {
         return BIZ_LIKE + SPLIT + String.valueOf(entityType) + SPLIT + String.valueOf(entityId);
@@ -22,5 +30,22 @@ public class RedisKeyUtil {
     public static String getEventQueueKey() {
         return BIZ_EVENTQUEUE;
     }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // 某个实体的粉丝key
+    public static String getFollowerKey(int entityType, int entityId) {
+        return BIZ_FOLLOWER + SPLIT + String.valueOf(entityType) + SPLIT + String.valueOf(entityId);
+    }
+
+    // 每个用户对某类实体的关注key
+    public static String getFolloweeKey(int userId, int entityType) {
+        return BIZ_FOLLOWEE + SPLIT + String.valueOf(userId) + SPLIT + String.valueOf(entityType);
+    }
+
+    public static String getTimelineKey(int userId) {
+        return BIZ_TIMELINE + SPLIT + String.valueOf(userId);
+    }
+
 
 }
